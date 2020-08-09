a242613bf08c0d65a23e86174556dffe621ef865
cccbeginner <arvin.tsai.0113@gmail.com>
2020-08-09 15:31:58 +0800

    use .env file

--- web/routes/submissions.js
+++ web/routes/submissions.js
@@ -8,12 +8,19 @@ const router = express.Router();
 /* GET submission list. */
 router.get('/', Submissions.list, async (req, res) => {
   // get username & problem's title first
-  for (let key = 0; key < req.submissions.length; key += 1) {
+  /*for (let key = 0; key < req.submissions.length; key += 1) {
     req.params.user_id = req.submissions[key].submitter_id;
     req.params.problem_id = req.submissions[key].problem_id;
     req.submissions[key].submitter = (await UserInfo.retrieve(req, res)).username;
     req.submissions[key].problemTitle = (await Problems.retrieve(req, res)).title;
-  }
+  }*/
+
+  req.submissions.forEach(async submission => {
+    req.params.user_id = req.submission.submitter_id;
+    req.params.problem_id = req.submission.problem_id;
+    req.submission.submitter = (await UserInfo.retrieve(req, res)).username;
+    req.submission.problemTitle = (await Problems.retrieve(req, res)).title;
+  });
 
   res.render('submission/index', {
     submissions: req.submissions,
