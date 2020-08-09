eb501fae777f0beff1ff24fec20bb686cd6053b9
cccbeginner <arvin.tsai.0113@gmail.com>
2020-08-09 22:16:36 +0800

    fix last commit mistakes

--- web/routes/submissions.js
+++ web/routes/submissions.js
@@ -7,27 +7,21 @@ const router = express.Router();
 
 /* GET submission list. */
 router.get('/', Submissions.list, async (req, res) => {
-  // get username & problem's title first
-  /*for (let key = 0; key < req.submissions.length; key += 1) {
-    req.params.user_id = req.submissions[key].submitter_id;
-    req.params.problem_id = req.submissions[key].problem_id;
-    req.submissions[key].submitter = (await UserInfo.retrieve(req, res)).username;
-    req.submissions[key].problemTitle = (await Problems.retrieve(req, res)).title;
-  }*/
-
-  req.submissions.forEach(async submission => {
-    req.params.user_id = req.submission.submitter_id;
-    req.params.problem_id = req.submission.problem_id;
-    req.submission.submitter = (await UserInfo.retrieve(req, res)).username;
-    req.submission.problemTitle = (await Problems.retrieve(req, res)).title;
+  var names = req.submissions.map(async (submission) => {
+    req.params.user_id = submission.submitter_id;
+    req.params.problem_id = submission.problem_id;
+    submission.submitter = (await UserInfo.retrieve(req, res)).username;
+    submission.problemTitle = (await Problems.retrieve(req, res)).title;
   });
 
-  res.render('submission/index', {
-    submissions: req.submissions,
-    isAdmin: req.isAdmin,
-    isLogin: req.isLogin,
-    cur_user: req.session.user,
-  });
+  Promise.all(names).then(() => {
+    res.render('submission/index', {
+      submissions: req.submissions,
+      isAdmin: req.isAdmin,
+      isLogin: req.isLogin,
+      cur_user: req.session.user,
+    });
+  })
 });
 
 /* GET submission detail */
